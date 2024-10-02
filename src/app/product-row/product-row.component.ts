95244b258f96c8c724d1a03ca0d0c8aa47439f1b
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-10-02 18:42:26 +0400

    add some files

---
+++ src/app/product-row/product-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-row.component.html',
+  styleUrl: './product-row.component.css'
+})
+export class ProductRowComponent {
+
+}
