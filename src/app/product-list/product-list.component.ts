f3b7a25845043a81825a4244100bf9c605f60871
GigiGiorgadze10 <giorgadze.gigi2004@gmail.com>
2024-10-02 19:38:23 +0400

    some changes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,6 +16,8 @@ export class ProductListComponent {
     { sku: 'NEATOJACKET', name: 'nike T-sheert', imageUrl: 'https://via.placeholder.com/150', department: ['Women', 'T-sheerts'], price: 23.99 },
     { sku: 'NICEHAT', name: 'Mercedes F1 white Hat', imageUrl: 'https://via.placeholder.com/150', department: ['Men', 'Accessories'], price: 29.99 },
     { sku: 'SPORTHAT', name: 'racing helmet', imageUrl: 'https://via.placeholder.com/150', department: ['Men', 'Accessories'], price: 200.00 },
+    { sku: 'MYSHOES', name: 'green adidas gazelle', imageUrl: 'https://via.placeholder.com/150', department: ['Men', 'Shoes'], price: 119.99 },
+    { sku: 'NEATOJACKET', name: 'nike T-sheert', imageUrl: 'https://via.placeholder.com/150', department: ['Women', 'T-sheerts'], price: 23.99 },
   ];
 
   constructor() {
